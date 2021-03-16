46c0f1cfd02cf29b15e1b5def8033e034da81c49
mdnajmul <mdnajmulovi@gmail.com>
2021-03-16 08:40:48 +0600

    adding user login & jwt signing

--- api/routes/users.js
+++ api/routes/users.js
@@ -13,6 +13,9 @@ const User = require('../models/user');
 //import bcrypt package
 const bcrypt = require('bcrypt');
 
+//import jsonwebtoken package
+const jwt = require('jsonwebtoken');
+
 
 //signup/register users
 router.post('/signup', (req, res, next) => {
@@ -53,6 +56,53 @@ router.post('/signup', (req, res, next) => {
             }
         })
 });
+
+
+//signin/login user
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if(user.length < 1) {
+                return res.status(401).json({
+                    message: 'Auth Failed'
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth Failed'
+                    });
+                }
+                if (result) {
+                   const token = jwt.sign(
+                            {
+                                email: user[0].email,
+                                userId: user[0]._id
+                            }, 
+                            process.env.JWT_KEY,
+                            {
+                                expiresIn: '1h'
+                            }
+                        )
+                    return res.status(200).json({
+                        message: 'Auth Successful',
+                        token: token
+                    });
+                }
+                res.status(401).json({
+                    message: 'Auth Failed'
+                });
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
 
 
 //delete users
